01b34dd6f809ea465edabb81c2c9bd02bc918b08
Sofian Hammou <sofianhammou30@gmail.com>
2024-04-07 18:12:38 -0700

    simple arithmetic in js

---
+++ test.js
@@ -0,0 +1,20 @@
+/* 3 Examples of basic arithmetic in JS */
+
+console.log("2+5: 7");
+console.log("3*3: 9");
+console.log("4/2: 2");
+
+/* 2 Examples of declaring variables in JS */
+
+var one = 1;
+var two = 2;
+
+
+/* 2 Examples of using variables in arithmetic operations in JS */
+
+var add = one + two;
+console.log("additon: " + add);
+var multiply = one * two;
+console.log("multiplication: " + multiply);
+
+
